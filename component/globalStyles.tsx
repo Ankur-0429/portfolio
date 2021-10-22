9e9753ff895d9e9c484de67fc6c8c82961e0bcec
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-22 09:15:26 -0700

    removed ts errors

--- component/globalStyles.tsx
+++ component/globalStyles.tsx
@@ -1,9 +1,9 @@
-import { createGlobalStyle} from "styled-components"
+import { createGlobalStyle, DefaultTheme} from "styled-components"
 
 export const GlobalStyles = createGlobalStyle`
   body {
-    background: ${({ theme }) => theme.body};
-    color: ${({ theme }) => theme.text};
+    background: ${({ theme }:any) => theme.body};
+    color: ${({ theme }:any) => theme.text};
     font-family: Tahoma, Helvetica, Arial, Roboto, sans-serif;
     transition: all 0.30s linear;
   }  
