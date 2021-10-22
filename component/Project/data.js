594bba7e69faa9fa3e5984088b1c3ce909510703
Ankur Ahir <ankurahir0429@gmail.com>
2021-10-22 09:30:14 -0700

    changed relative path to images projects

--- component/Project/data.js
+++ component/Project/data.js
@@ -1,19 +1,19 @@
 const data = [
     {
         name: 'Project 4',
-        image: '/../public/pathfinder.jpg',
+        image: '/pathfinder.jpg',
         url: 'https://astaralgorithm.ankurahir.repl.co/',
         detail: 'my Details'
     },
     {
         name: 'Project 1',
-        image: '/../public/pathfinder.jpg',
+        image: '/pathfinder.jpg',
         url: 'https://astaralgorithm.ankurahir.repl.co/',
         detail: 'my Details'
     },
     {
         name: 'Project 5',
-        image: '/../public/pathfinder.jpg',
+        image: '/pathfinder.jpg',
         url: 'https://astaralgorithm.ankurahir.repl.co/',
         detail: 'my Details'
     }
