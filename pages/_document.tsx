7a37c484a206da4087ac114c1bb2620104306f87
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-01-04 22:23:44 -0800

    changed description and title

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,11 +22,10 @@ class MyDocument extends Document {
           <link rel="icon" href="/favicon.ico" />
           <meta name="description" content="Ankur Ahir Portfolio Website" />
           <meta name="keywords" content="Ankur Ahir Software Developer DevOps Engineer Portfolio Site" />
-          <meta name="title" property="og:title" content="Ankur Ahir DevOps Engineer Portfolio Website" />
+          <meta name="title" property="og:title" content="Portfolio Website" />
           <meta property="og:type" content="Website" />
           <meta name="image" property="og:image" content="https://i.imgur.com/gk31YMY.png" />
           <meta property="og:url" content="https://portfolio-ankur-0429.vercel.app/" />
-          <meta name="description" property="og:description" content="DevOps Engineer Portfolio Website for Ankur Ahir" />
           <meta name="author" content="Ankur Ahir" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
 
