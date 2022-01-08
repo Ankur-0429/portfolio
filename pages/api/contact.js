674f47f19ee4391e3d8fd4ebd8314a25fda4ae87
Ankur Ahir <ahir.ankur0429@gmail.com>
2022-01-08 12:46:21 -0800

    added oatch creds for nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,48 +1,49 @@
 const nodemailer = require("nodemailer");
 
 export default async (req, res) => {
-
-const transporter = nodemailer.createTransport({
-    port: 465,
-    host: "smtp.gmail.com",
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
     auth: {
-        user: process.env.user,
-        pass: process.env.password,
+      type: "OAuth2",
+      user: process.env.MAIL_USERNAME,
+      pass: process.env.MAIL_PASSWORD,
+      clientId: process.env.OAUTH_CLIENTID,
+      clientSecret: process.env.OAUTH_CLIENT_SECRET,
+      refreshToken: process.env.OAUTH_REFRESH_TOKEN,
     },
-    secure: true,
-});
+  });
 
-await new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     // verify connection configuration
     transporter.verify(function (error, success) {
-        if (error) {
-            console.log(error);
-            reject(error);
-        } else {
-            resolve(success);
-        }
+      if (error) {
+        console.log(error);
+        reject(error);
+      } else {
+        resolve(success);
+      }
     });
-});
+  });
 
-const mailData = {
-    from: process.env.user,
-    to: process.env.user,
+  const mailData = {
+    from: process.env.USER,
+    to: process.env.USER,
     subject: req.body.subject,
     text: req.body.message + " | Sent from: " + req.body.email,
     html: `<div>${req.body.message}</div><p>Sent from:
     ${req.body.email}</p>`,
-};
+  };
 
-await new Promise((resolve, reject) => {
+  await new Promise((resolve, reject) => {
     // send mail
     transporter.sendMail(mailData, (err, info) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve(info);
-        }
+      if (err) {
+        reject(err);
+      } else {
+        resolve(info);
+      }
     });
-});
+  });
 
-res.status(200).json({ status: "OK" });
+  res.status(200).json({ status: "OK" });
 };
